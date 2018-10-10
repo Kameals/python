6ad49215c9463680d44beca705c0d868aad4d468
Kameals <truekhm@gmail.com>
2018-10-10 23:48:11 +0900

    .

--- app2.py
+++ app2.py
@@ -1,6 +1,6 @@
 
 character_name = "Tom"
-character_age = 50.19234
+character_age = 50
 is_male = False
 print("There once was a man named " + character_name + ", ")
 print("he ws " + character_age + " years old. ")
@@ -9,3 +9,4 @@ character_name = "Mike"
 character_age = "20"
 print("He really liked the name " + character_name + ", ")
 print("but didn't like being " + character_age + ".")
+
